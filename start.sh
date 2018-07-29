4bf22dc4f9fcbb68bff7c7dd2a6881c1fd8c5acb
Joseph Hughes <jjhughes57@gmail.com>
2018-07-29 17:31:53 -0400

    fix: debug

--- start.sh
+++ start.sh
@@ -3,4 +3,6 @@
 cp -r /cache/Linux_for_Tegra /workdir/jetson-flash-artifacts/ || exit 1
 cp /cache/Linux_for_Tegra.stamp /workdir/jetson-flash-artifacts/Linux_for_Tegra.stamp || exit 1
 
-bin/cmd.js -p -o /workdir -f $1
+echo "here is what happened..."
+ls -la /workdir/jetson-flash-artifacts/
+#bin/cmd.js -p -o /workdir -f $1
